5105d6a93b1e9781934dc14f6a3cf7a9a08825a7
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-06-06 08:46:17 +0200

    done

---
+++ Script/code.js
@@ -0,0 +1,10 @@
+let Answer = document.getElementById("Output")
+let Display = document.getElementById("Seconds")
+let NumberSet = [1, 2, 4, 5].reduce((SumONumbers, a ) => SumONumbers + a , 0)
+function ArrayAdder() {
+    
+console.log(NumberSet)
+
+
+
+}
